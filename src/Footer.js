019e4e6b0a677942699eb1aed5fd32f06e744e74
Sabrina-MM <sabrinamarisolmartinez@gmail.com>
2020-09-22 20:55:28 +0100

    added spaces

--- src/Footer.js
+++ src/Footer.js
@@ -3,15 +3,16 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer">
+      This project was coded by Sabrina MM and is{" "}
       <a
         href="https://github.com/Sabrina-MM/weather-react"
         className="bottom-link"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Open-source code,
-      </a>
-      <i>by Sabrina Marisol Martinez</i>
+        Open-sourced on GitHub
+      </a>{" "}
+      and hosted on Netlify
     </div>
   );
 }
